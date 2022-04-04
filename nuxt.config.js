08c7811ffe580b9180c1c2c8d0989b7bd8383ed7
Matheus Carvalho <47832724+matheuscarvalho23@users.noreply.github.com>
2022-04-04 18:12:55 -0300

    App Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'desafio-frontend',
+    title: 'Your Video',
     htmlAttrs: {
       lang: 'en'
     },
